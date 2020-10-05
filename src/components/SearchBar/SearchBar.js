ef5b963f1cacbc1940bcdfcc15a6a71aab9e154d
Nikolay Schekin <ns.datascientist@gmail.com>
2020-10-05 21:44:46 +0300

    SearchBar component under development

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchBar.css';
+
+sortByOptions = {
+    'Best Match': undefined,
+    'Highest Rated': undefined,
+    'Most Reviewed': undefined
+};
+
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+};
