aabf9895258226430f472b198bad936f5bf63b89
Nikolay Schekin <ns.datascientist@gmail.com>
2020-10-06 12:59:11 +0300

    render sortByOptions method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './SearchBar.css';
 
 sortByOptions = {
-    'Best Match': undefined,
-    'Highest Rated': undefined,
-    'Most Reviewed': undefined
+    'Best Match': "best_match",
+    'Highest Rated': "rating",
+    'Most Reviewed': "review_count"
 };
 
 
 class SearchBar extends React.Component {
-    render() {
-        return (
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+        return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
+    // render() {
+    //     return (
 
-        )
+    //     )
     }
 };
