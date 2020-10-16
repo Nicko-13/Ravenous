ba800512efc68128c3e0205e42138e03b6cc01d5
Nikolay Schekin <ns.datascientist@gmail.com>
2020-10-16 20:20:15 +0300

    step 26

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -10,8 +10,25 @@ const Yelp = {
                 })
                 .then(jsonResponse => {
                     if (jsonResponse.businesses) {
-                        
+                        jsonResponse.businesses.map(
+                            business => {
+                                return {
+                                    'id': business['id'], 
+                                    'imageSrc': business['image_url'], 
+                                    'name': business['name'],
+                                    'address': business['location']['address1'],
+                                    'city': business['location']['city'],
+                                    'state': business['location']['state'],
+                                    'zipCode': business['location']['zip_code'],
+                                    'category': business[0]['title'],
+                                    'rating': business['rating'],
+                                    'reviewCount': business['review_count']
+                                };
+                            }
+                        );
                     }
                 });
     }
 };
+
+export default Yelp;
